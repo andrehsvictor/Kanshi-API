01b4854214271b1df401e6267ee734e5937b5c70
André Victor <andrehsvictor@gmail.com>
2024-05-22 20:07:53 -0300

    first commit

---
+++ src/main/java/andrehsvictor/kanshi/KanshiApplication.java
@@ -0,0 +1,13 @@
+package andrehsvictor.kanshi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanshiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanshiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/andrehsvictor/kanshi/KanshiApplicationTests.java
@@ -0,0 +1,13 @@
+package andrehsvictor.kanshi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanshiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
