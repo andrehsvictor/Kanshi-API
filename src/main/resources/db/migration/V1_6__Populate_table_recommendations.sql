1b7ec5831a265f0cc789ca790e03e339549b57db
André Victor <andrehsvictor@gmail.com>
2024-05-22 20:42:18 -0300

    feat: Add SQL script to populate recommendations table
    This commit adds a new SQL script `V1_6__Populate_table_recommendations.sql` that populates the `recommendations` table in the database with sample data for user recommendations on anime titles.

---
+++ src/main/resources/db/migration/V1_6__Populate_table_recommendations.sql
@@ -0,0 +1,33 @@
+INSERT INTO
+    recommendations (user_id, anime_id, rating, comment)
+VALUES
+    (
+        1,
+        1,
+        9.0,
+        'Highly recommended! Great storyline and intense action.'
+    ),
+    (
+        1,
+        2,
+        8.5,
+        'Intriguing plot with mind games. A must-watch.'
+    ),
+    (
+        2,
+        3,
+        9.5,
+        'Masterpiece! Fantastic character development and emotional depth.'
+    ),
+    (
+        2,
+        4,
+        8.0,
+        'Hilarious and unique superhero concept.'
+    ),
+    (
+        1,
+        5,
+        7.5,
+        'Interesting virtual world adventure with romance.'
+    );
