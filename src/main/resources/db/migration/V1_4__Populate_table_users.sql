9660faf5fbc7e769f27e74c068a6c4ece2a37f90
André Victor <andrehsvictor@gmail.com>
2024-05-22 20:42:02 -0300

    feat: Add SQL script to populate users table
    The commit adds a new SQL script `V1_4__Populate_table_users.sql` that populates the `users` table in the database with sample data.

---
+++ src/main/resources/db/migration/V1_4__Populate_table_users.sql
@@ -0,0 +1,13 @@
+INSERT INTO
+    users (username, password, email)
+VALUES
+    (
+        'andrehsv',
+        '$2a$12$3aq5dgKpNkXXeye19N0UXeCytB9H4./IgsZvtqeDZ49LAI/Jojn8i',
+        'andrehsvictor@kanshi.io'
+    ),
+    (
+        'josecarlos',
+        '$2a$12$3aq5dgKpNkXXeye19N0UXeCytB9H4./IgsZvtqeDZ49LAI/Jojn8i',
+        'jcarlos@kanshi.io'
+    );
