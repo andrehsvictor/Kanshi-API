49a04377de64e5d7494fd99faf3b383f8beaaa2e
André Victor <andrehsvictor@gmail.com>
2024-05-22 20:42:46 -0300

    feat: Add SQL script to create likes table
    This commit adds a new SQL script `V2_0__Create_table_likes.sql` that creates the `likes` table in the database. The table includes columns for `id`, `user_id`, `anime_id`, `created_at`, and `updated_at`. The `user_id` column references the `users` table and the `anime_id` column references the `animes` table.

---
+++ src/main/resources/db/migration/V2_0__Create_table_likes.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS likes (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    anime_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (anime_id) REFERENCES animes (id)
+);
