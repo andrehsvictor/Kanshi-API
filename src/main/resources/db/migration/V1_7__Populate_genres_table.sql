0815c339d2c5ba32bcee1bb86fc94508995e308a
André Victor <andrehsvictor@gmail.com>
2024-05-22 20:42:25 -0300

    feat: Add SQL script to populate genres table

---
+++ src/main/resources/db/migration/V1_7__Populate_genres_table.sql
@@ -0,0 +1,16 @@
+INSERT INTO
+    genres (name)
+VALUES
+    ('Action'),
+    ('Horror'),
+    ('Fantasy'),
+    ('Sci-Fi'),
+    ('Romance'),
+    ('Comedy'),
+    ('Adventure'),
+    ('Supernatural'),
+    ('Psychological'),
+    ('Mystery'),
+    ('Sports'),
+    ('Historical'),
+    ('Slice of Life');
