a59b339be2d107ac8b86c640d0f02cfd08bb54c5
André Victor <andrehsvictor@gmail.com>
2024-05-23 09:58:10 -0300

    feat: Add ShortRecommendationDTO for managing short recommendation data

---
+++ src/main/java/andrehsvictor/kanshi/dto/ShortRecommendationDTO.java
@@ -0,0 +1,45 @@
+package andrehsvictor.kanshi.dto;
+
+public class ShortRecommendationDTO {
+    private Long id;
+    private UserDTO user;
+    private String comment;
+    private Double rating;
+
+    public ShortRecommendationDTO() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public UserDTO getUser() {
+        return user;
+    }
+
+    public void setUser(UserDTO user) {
+        this.user = user;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public void setRating(Double rating) {
+        this.rating = rating;
+    }
+
+    
+}
