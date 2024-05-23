7646114bb756a243a8081d6ae09cfe511dbcb451
André Victor <andrehsvictor@gmail.com>
2024-05-22 21:33:59 -0300

    feat: Add Genre entity class
    This commit adds a new entity class `Genre` in the `andrehsvictor.kanshi.entity` package. The `Genre` class represents a genre of anime and includes properties such as `id`, `name`, `createdAt`, `updatedAt`, and `animes`. This class is necessary for storing and retrieving genre data in the application's database.

---
+++ src/main/java/andrehsvictor/kanshi/entity/Genre.java
@@ -0,0 +1,103 @@
+package andrehsvictor.kanshi.entity;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "genres")
+public class Genre {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @ManyToMany(mappedBy = "genres")
+    private Set<Anime> animes;
+
+    public Genre() {
+    }
+
+    public Genre(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Set<Anime> getAnimes() {
+        return animes;
+    }
+
+    public void setAnimes(Set<Anime> animes) {
+        this.animes = animes;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Genre other = (Genre) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Genre [id=" + id + ", name=" + name + ", createdAt=" + createdAt + ", updatedAt=" + updatedAt + "]";
+    }
+}
