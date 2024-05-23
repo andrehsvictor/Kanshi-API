67f848bd718533eca101cec68bf86cd94822c9d3
André Victor <andrehsvictor@gmail.com>
2024-05-22 21:35:00 -0300

    feat: Implement UserDetailsService for loading user details during authentication
    This commit adds a new class `UserDetailsServiceImpl` in the `andrehsvictor.kanshi.security` package. The `UserDetailsServiceImpl` class implements the `UserDetailsService` interface and overrides the `loadUserByUsername` method to load user details from the database based on the provided username. It uses the `UserRepository` to find the user by email and throws a `UsernameNotFoundException` if the user is not found. The loaded user details are then wrapped in a `UserDetailsImpl` object and returned. This implementation is necessary for authenticating users using Spring Security.

---
+++ src/main/java/andrehsvictor/kanshi/security/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package andrehsvictor.kanshi.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import andrehsvictor.kanshi.entity.User;
+import andrehsvictor.kanshi.repository.UserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + username));
+        return new UserDetailsImpl(user);
+    }
+
+}
