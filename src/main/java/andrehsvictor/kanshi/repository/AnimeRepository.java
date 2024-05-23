951dc211382ce6c0c27026f645daed34353413f9
André Victor <andrehsvictor@gmail.com>
2024-05-23 09:58:16 -0300

    feat: Add AnimeRepository interface for managing anime data

--- src/main/java/andrehsvictor/kanshi/repository/AnimeRepository.java
+++ src/main/java/andrehsvictor/kanshi/repository/AnimeRepository.java
@@ -1,7 +1,6 @@
 package andrehsvictor.kanshi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import andrehsvictor.kanshi.entity.Anime;
 
 public interface AnimeRepository extends JpaRepository<Anime, Long> {
