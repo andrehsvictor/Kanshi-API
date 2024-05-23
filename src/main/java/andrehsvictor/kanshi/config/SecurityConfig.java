983f7c35e8943024aefdaa0488e643ca403c4614
André Victor <andrehsvictor@gmail.com>
2024-05-23 08:32:24 -0300

    feat: Update SecurityConfig for JWT token handling
    This commit updates the `SecurityConfig` class to handle JWT token authentication and authorization. It adds the necessary dependencies for JWT token encoding and decoding, including the `RSAPrivateKey` and `RSAPublicKey` properties. It also configures the `JwtDecoder` and `JwtEncoder` beans for token verification and signing. Additionally, it implements the `AuthenticationManager` bean for authentication and the `PasswordEncoder` bean for password encoding. These changes are essential for implementing secure authentication and authorization using JSON Web Tokens.

--- src/main/java/andrehsvictor/kanshi/config/SecurityConfig.java
+++ src/main/java/andrehsvictor/kanshi/config/SecurityConfig.java
@@ -1,18 +1,43 @@
 package andrehsvictor.kanshi.config;
 
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Value("${kanshi.jwt.private.key}")
+    private RSAPrivateKey privateKey;
+
+    @Value("${kanshi.jwt.public.key}")
+    private RSAPublicKey publicKey;
+
     private final String[] PUBLIC_ROUTES = {
             "/v3/api-docs/**",
             "/swagger-ui.html",
@@ -28,11 +53,36 @@ public class SecurityConfig {
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(c -> c.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(c -> c.requestMatchers(HttpMethod.POST, AUTHENTICATION_ROUTES).permitAll()
-                        .requestMatchers(PUBLIC_ROUTES).permitAll()
-                        .anyRequest().authenticated())
-                .oauth2ResourceServer(c -> c.jwt(Customizer.withDefaults()))
+                .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(
+                        auth -> auth.requestMatchers(HttpMethod.POST, AUTHENTICATION_ROUTES).permitAll()
+                                .requestMatchers(PUBLIC_ROUTES).permitAll()
+                                .anyRequest().authenticated())
+                .oauth2ResourceServer(o -> o.jwt(jwt -> jwt.decoder(jwtDecoder())))
                 .build();
     }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    JwtEncoder jwtEncoder() {
+        JWK jwk = new RSAKey.Builder(publicKey).privateKey(privateKey).build();
+        JWKSource<SecurityContext> jwkSet = new ImmutableJWKSet<>(new JWKSet(jwk));
+        return new NimbusJwtEncoder(jwkSet);
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withPublicKey(publicKey).build();
+    }
+
 }
