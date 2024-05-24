71bdfee864b0023859a6ae7fcb80596964383ba2
André Victor <andrehsvictor@gmail.com>
2024-05-24 11:20:30 -0300

    feat: Add OpenAPI configuration to AnimeController
    This commit adds the necessary OpenAPI annotations to the `AnimeController` class to enable API documentation using Swagger. It includes the `@Tag` annotation to categorize the controller as "Anime" and the `@Operation` annotations to provide summaries and responses for each endpoint. This addition enhances the documentation and visualization of the API endpoints.

--- src/main/java/andrehsvictor/kanshi/controller/AnimeController.java
+++ src/main/java/andrehsvictor/kanshi/controller/AnimeController.java
@@ -15,30 +15,47 @@ import org.springframework.web.bind.annotation.RestController;
 import andrehsvictor.kanshi.dto.AnimeDTO;
 import andrehsvictor.kanshi.dto.ShortAnimeDTO;
 import andrehsvictor.kanshi.service.AnimeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
+@Tag(name = "Anime")
 public class AnimeController {
 
     @Autowired
     private AnimeService animeService;
 
     @GetMapping("/api/1.0/animes")
+    @Operation(summary = "Get all animes", responses = {
+            @ApiResponse(responseCode = "200", description = "Animes returned successfully")
+    })
     public ResponseEntity<Page<ShortAnimeDTO>> findAll(Pageable pageable) {
         return ResponseEntity.ok(animeService.findAll(pageable));
     }
 
     @GetMapping("/api/1.0/animes/{id}")
+    @Operation(summary = "Get anime by id", responses = {
+            @ApiResponse(responseCode = "200", description = "Anime returned successfully"),
+            @ApiResponse(responseCode = "404", description = "Anime not found")
+    })
     public ResponseEntity<AnimeDTO> findById(@PathVariable Long id) {
         return ResponseEntity.ok(animeService.findById(id));
     }
 
     @PatchMapping("/api/1.0/animes/{id}/like")
+    @Operation(summary = "Like an anime", responses = {
+            @ApiResponse(responseCode = "200", description = "Anime liked successfully")
+    })
     public ResponseEntity<Void> like(@PathVariable Long id, JwtAuthenticationToken token) {
         animeService.like(id, token);
         return ResponseEntity.ok().build();
     }
 
     @GetMapping("/api/1.0/animes/liked")
+    @Operation(summary = "Get liked animes", responses = {
+            @ApiResponse(responseCode = "200", description = "Liked animes returned successfully")
+    })
     public ResponseEntity<Set<ShortAnimeDTO>> findLikedAnimes(JwtAuthenticationToken token) {
         return ResponseEntity.ok(animeService.findLikedAnimes(token));
     }
