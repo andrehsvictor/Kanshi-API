4bb4b2bac94f96d3de3e8342b1f6b32e743452ce
André Victor <andrehsvictor@gmail.com>
2024-05-24 11:20:45 -0300

    feat: Add OpenAPI annotations for RecommendationController
    This commit adds the necessary OpenAPI annotations to the `RecommendationController` class to enable API documentation using Swagger. It includes the `@Operation` and `@ApiResponse` annotations with summaries and responses for each endpoint. This addition enhances the documentation and visualization of the API endpoints for recommendations.

--- src/main/java/andrehsvictor/kanshi/controller/RecommendationController.java
+++ src/main/java/andrehsvictor/kanshi/controller/RecommendationController.java
@@ -16,41 +16,68 @@ import org.springframework.web.bind.annotation.RestController;
 import andrehsvictor.kanshi.dto.RecommendationDTO;
 import andrehsvictor.kanshi.dto.SaveOrUpdateRecommendationDTO;
 import andrehsvictor.kanshi.service.RecommendationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
+@Tag(name = "Recommendation")
 public class RecommendationController {
 
     @Autowired
     private RecommendationService recommendationService;
 
     @GetMapping("/api/1.0/recommendations")
+    @Operation(summary = "Get all recommendations", responses = {
+            @ApiResponse(responseCode = "200", description = "Recommendations returned successfully")
+    })
     public ResponseEntity<Page<RecommendationDTO>> findAll(Pageable pageable) {
         return ResponseEntity.ok(recommendationService.findAll(pageable));
     }
 
     @GetMapping("/api/1.0/recommendations/{id}")
+    @Operation(summary = "Get recommendation by id", responses = {
+            @ApiResponse(responseCode = "200", description = "Recommendation returned successfully"),
+            @ApiResponse(responseCode = "404", description = "Recommendation not found")
+    })
     public ResponseEntity<RecommendationDTO> findById(@PathVariable Long id) {
         return ResponseEntity.ok(recommendationService.findById(id));
     }
 
     @GetMapping("/api/1.0/animes/{animeId}/recommendations/all")
+    @Operation(summary = "Get all recommendations by anime id", responses = {
+            @ApiResponse(responseCode = "200", description = "Recommendations returned successfully")
+    })
     public ResponseEntity<Page<RecommendationDTO>> findAllByAnimeId(@PathVariable Long animeId, Pageable pageable) {
         return ResponseEntity.ok(recommendationService.findAllByAnimeId(animeId, pageable));
     }
 
     @PostMapping("/api/1.0/animes/{animeId}/recommendations")
+    @Operation(summary = "Recommend an anime", responses = {
+            @ApiResponse(responseCode = "200", description = "Anime recommended successfully"),
+            @ApiResponse(responseCode = "404", description = "Anime not found"),
+            @ApiResponse(responseCode = "400", description = "Invalid recommendation")
+    })
     public ResponseEntity<RecommendationDTO> recommend(@PathVariable Long animeId, JwtAuthenticationToken token,
             @RequestBody SaveOrUpdateRecommendationDTO saveRecommendationDTO) {
         return ResponseEntity.ok(recommendationService.save(saveRecommendationDTO, animeId, token));
     }
 
     @DeleteMapping("/api/1.0/recommendations/{id}")
+    @Operation(summary = "Delete recommendation by id", responses = {
+            @ApiResponse(responseCode = "204", description = "Recommendation deleted successfully"),
+            @ApiResponse(responseCode = "404", description = "Recommendation not found")
+    })
     public ResponseEntity<Void> delete(@PathVariable Long id, JwtAuthenticationToken token) {
         recommendationService.delete(id, token);
         return ResponseEntity.noContent().build();
     }
 
     @PutMapping("/api/1.0/recommendations/{id}")
+    @Operation(summary = "Update recommendation by id", responses = {
+            @ApiResponse(responseCode = "200", description = "Recommendation updated successfully"),
+            @ApiResponse(responseCode = "404", description = "Recommendation not found")
+    })
     public ResponseEntity<RecommendationDTO> update(@PathVariable Long id, JwtAuthenticationToken token,
             @RequestBody SaveOrUpdateRecommendationDTO updateRecommendationDTO) {
         return ResponseEntity.ok(recommendationService.update(id, updateRecommendationDTO, token));
