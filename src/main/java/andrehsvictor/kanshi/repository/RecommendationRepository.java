1aca0b2924d436e58f4c1f7fad0bb9c3d98aa756
André Victor <andrehsvictor@gmail.com>
2024-05-24 10:02:02 -0300

    feat: Add lazy loading for images in RecommendationRepository

--- src/main/java/andrehsvictor/kanshi/repository/RecommendationRepository.java
+++ src/main/java/andrehsvictor/kanshi/repository/RecommendationRepository.java
@@ -1,9 +1,17 @@
 package andrehsvictor.kanshi.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import andrehsvictor.kanshi.entity.Recommendation;
 
 public interface RecommendationRepository extends JpaRepository<Recommendation, Long> {
-    
+    @Query(nativeQuery = true, value = """
+            SELECT recommendations.*
+            FROM recommendations
+            JOIN animes ON recommendations.anime_id = :animeId
+            """)
+    Page<Recommendation> findAllByAnimeId(Long animeId, Pageable pageable);
 }
