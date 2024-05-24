9f131e484ab216d7179282a586911ab87ae3a963
André Victor <andrehsvictor@gmail.com>
2024-05-24 11:20:38 -0300

    feat: Add OpenAPI annotations for user authentication endpoint
    This commit adds the necessary OpenAPI annotations to the `AuthenticationController` class to enable API documentation using Swagger. It includes the `@Operation` annotation with a summary and responses for the `authenticate` endpoint. This addition enhances the documentation and visualization of the API endpoint for user authentication.

--- src/main/java/andrehsvictor/kanshi/controller/AuthenticationController.java
+++ src/main/java/andrehsvictor/kanshi/controller/AuthenticationController.java
@@ -9,14 +9,22 @@ import org.springframework.web.bind.annotation.RestController;
 import andrehsvictor.kanshi.dto.AuthenticateDTO;
 import andrehsvictor.kanshi.dto.JwtDTO;
 import andrehsvictor.kanshi.service.AuthenticationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
+@Tag(name = "Authentication")
 public class AuthenticationController {
 
     @Autowired
     private AuthenticationService authenticationService;
 
     @PostMapping("/api/1.0/authenticate")
+    @Operation(summary = "Authenticate user", responses = {
+            @ApiResponse(responseCode = "200", description = "User authenticated successfully"),
+            @ApiResponse(responseCode = "401", description = "Invalid credentials")
+    })
     public ResponseEntity<JwtDTO> authenticate(@RequestBody AuthenticateDTO authenticateDTO) {
         return ResponseEntity.ok(authenticationService.authenticate(authenticateDTO));
     }
