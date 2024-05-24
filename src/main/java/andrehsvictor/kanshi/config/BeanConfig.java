a62a2de70e4c81dfc623cda0edaf016b5048fb82
André Victor <andrehsvictor@gmail.com>
2024-05-24 11:20:22 -0300

    feat: Add OpenAPI configuration to BeanConfig
    This commit adds the necessary configuration to the `BeanConfig` class to enable OpenAPI documentation for the Kanshi API. It includes the creation of a custom `OpenAPI` bean with the title, version, and description of the API. This addition enhances the documentation and visualization of the API endpoints using Swagger UI.

--- src/main/java/andrehsvictor/kanshi/config/BeanConfig.java
+++ src/main/java/andrehsvictor/kanshi/config/BeanConfig.java
@@ -4,11 +4,20 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
 @Configuration
 public class BeanConfig {
 
     @Bean
     ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Kanshi API").version("1.0").description("API for Kanshi application"));
     }
 }
