e3e14235afc4e4891577bd6d9d8219db79fef27c
André Victor <andrehsvictor@gmail.com>
2024-05-24 15:58:27 -0300

    feat: Add AnimeRepository to AuthenticationControllerTest
    This commit adds the `AnimeRepository` to the `AuthenticationControllerTest` class. The repository is autowired and used in the test setup method to delete all anime records before each test. This change ensures a clean state for testing the authentication controller.

--- src/test/java/andrehsvictor/kanshi/controller/AuthenticationControllerTest.java
+++ src/test/java/andrehsvictor/kanshi/controller/AuthenticationControllerTest.java
@@ -7,10 +7,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 import andrehsvictor.kanshi.dto.AuthenticateDTO;
 import andrehsvictor.kanshi.entity.User;
+import andrehsvictor.kanshi.repository.AnimeRepository;
 import andrehsvictor.kanshi.repository.RecommendationRepository;
 import andrehsvictor.kanshi.repository.UserRepository;
 import andrehsvictor.kanshi.util.AbstractControllerTest;
-
 import static io.restassured.RestAssured.given;
 
 import static org.hamcrest.Matchers.notNullValue;
@@ -26,11 +26,15 @@ class AuthenticationControllerTest extends AbstractControllerTest {
     @Autowired
     RecommendationRepository recommendationRepository;
 
+    @Autowired
+    AnimeRepository animeRepository;
+
     User user;
 
     @BeforeEach
     void setUp() {
         recommendationRepository.deleteAll();
+        animeRepository.deleteAll();
         userRepository.deleteAll();
 
         user = new User("user1", passwordEncoder.encode("P4ssword"), "user1@kanshi.io");
