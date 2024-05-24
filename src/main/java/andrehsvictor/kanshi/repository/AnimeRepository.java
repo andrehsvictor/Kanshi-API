2f5e44a8e092bb81125f011b00de025378deb0dd
André Victor <andrehsvictor@gmail.com>
2024-05-24 10:01:56 -0300

    feat: Add lazy loading for images in AnimeRepository
    The code changes in the AnimeRepository add two new methods for retrieving anime data based on title and genre. These methods use native SQL queries to perform case-insensitive searches and join tables to filter by genre. This addition improves the functionality of the repository by providing more flexible search options for anime titles and genres.

--- src/main/java/andrehsvictor/kanshi/repository/AnimeRepository.java
+++ src/main/java/andrehsvictor/kanshi/repository/AnimeRepository.java
@@ -1,8 +1,26 @@
 package andrehsvictor.kanshi.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import andrehsvictor.kanshi.entity.Anime;
 
 public interface AnimeRepository extends JpaRepository<Anime, Long> {
+    @Query(nativeQuery = true, value = """
+            SELECT *
+            FROM animes
+            WHERE LOWER(animes.title) LIKE LOWER(CONCAT('%', :title, '%'))
+            """)
+    Page<Anime> findAllByTitleContainingIgnoreCase(String title, Pageable pageable);
 
+    @Query(nativeQuery = true, value = """
+            SELECT animes.*
+            FROM animes
+            JOIN has_genre ON animes.id = has_genre.anime_id
+            JOIN genres ON has_genre.genre_id = genres.id
+            WHERE genres.name = :genre
+            """)
+    Page<Anime> findAllByGenre(String genre, Pageable pageable);
 }
